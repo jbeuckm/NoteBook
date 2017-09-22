e231666f0bf8675e5dd75e787bde98a1f8435e99
joe beuckman <joebeuckman@gmail.com>
2017-09-22 21:45:49 +0200

    noteOn and of functions begun

--- notebook.hpp
+++ notebook.hpp
@@ -2,7 +2,8 @@ enum NB_Mode { highest, lowest, velocity };
 
 class Note {
 	public:
-		Note(char _pitch, char _velocity) : pitch(_pitch), velocity(_velocity) {
+		Note(char _pitch, char _velocity) : 
+			pitch(_pitch), velocity(_velocity) {
 			this->next = 0;
 		}
 		char pitch;
@@ -10,16 +11,21 @@ class Note {
 		Note *next;
 };
 
-
 class NoteBook {
 	public:
-		NoteBook(NB_Mode mode = velocity);
-		void noteOn(char pitch, char velocity);
+		NoteBook(NB_Mode _mode = velocity) : mode(_mode) { 
+			head = 0;
+		};
+		Note *noteOn(char _pitch, char _velocity);
+		Note *noteOff(char _pitch);
 		NB_Mode mode;
 	private:
 		Note *head;
 };
 
-NoteBook::NoteBook(NB_Mode mode) {
-	this->mode = mode;
+Note *NoteBook::noteOn(char _pitch, char _velocity) {
+	return head;
+}
+Note *NoteBook::noteOff(char _pitch) {
+	return head;
 }
